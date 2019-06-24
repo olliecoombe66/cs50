023ba7af4ad6b459d65957c3f251f9e8206803f1
Ollie Coombe <olliecoombe@gmail.com>
2019-06-24 17:47:17 +0100

    Update to way questions are checked
    Also draf reg/login

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import csv
 
 from flask import Flask, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 
 # Configure application
@@ -40,7 +41,6 @@ def difficulty():
 
 	if(request.method == "POST"):
 		difficulty = request.form.get("difficulty")
-		print(difficulty)
 
 		#connect to DB
 		connection = sqlite3.connect('birds.db')
@@ -72,9 +72,73 @@ def difficulty():
 
 		return render_template("/bird-test.html", bird_dictionary=result)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+	if request.method == "GET":
+		return render_template("register.html")
+
+	elif request.method=="POST":
+		username = request.form.get("username")
+		password = request.form.get("password")
+		confirm_password = request.form.get("confirm_password")
+		hash = generate_password_hash(password)
+
+		connection = sqlite3.connect('birds.db')
+		cursor = connection.cursor()
+		cursor.execute("INSERT INTO users (`user_name`, `password`,`level`) VALUES (?,?,?)", (username,password,1,))
+		connection.commit()
+
+
+		return render_template("difficulty.html")
+
+
+"""
+		if username == None:
+        	return(0)
+        elif password != confirm_password:
+        	return0
+        elif not new_user_id:
+            return 0
+        else:
+            session["user_id"] = new_user_id
+"""
+
+@app.route("/login", methods=["GET"])
+def login():
+	# Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        
+
+        # Ensure password was submitted
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return render_template("/difficulty.html")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
 @app.route("/", methods=["GET"])
 def get_index():
-	return render_template("/difficulty.html")
+	
+	return render_template("index.html")
 
 
 
